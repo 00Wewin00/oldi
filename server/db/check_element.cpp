cdc8f53e48f8785ee27bd88494369e0dc7918428
timur <timue255@Gmail.com>
2026-09-08 20:06:34 +0200

    I finished creat function(check_element)

--- server/db/check_element.cpp
+++ server/db/check_element.cpp
@@ -23,24 +23,75 @@ int check_element(string db_open,string table,string SearchTerm,string key_1,str
             case 0:{
                 string sql="SELECT "+SearchTerm+" FROM "+table+";";
                 if (sqlite3_prepare_v2(db,sql.c_str(),-1,&stmt,nullptr)==SQLITE_OK) {
-                
+                    int result_sql_query =sqlite3_step(stmt);
+                    switch(result_sql_query){
+                        case SQLITE_OK:{
+                            code=code_succes;
+                            break;
+                        }
+                        case SQLITE_DONE:{
+                            code =code_fail;
+                            break;
+                        }
+                        default:{
+                            code=code_eror;
+                            break;
+                        }
+                    }
                 }
                 break;
             }
             case 1:{
-                string sql="SELECT "+SearchTerm+" FROM "+table+" WHERE "+key_1+" = ?;";
+                string sql="SELECT "+SearchTerm+" FROM "+table+" WHERE "+key_1+" = :sqlfilter_2;";
                 if (sqlite3_prepare_v2(db,sql.c_str(),-1,&stmt,nullptr)==SQLITE_OK) {
-                
+                    int index_filter_1 = sqlite3_bind_parameter_index(stmt,":sqlfilter_1");
+                    sqlite3_bind_text(stmt,index_filter_1,filter_1.c_str(),-1,SQLITE_STATIC);
+                    int result_sql_query =sqlite3_step(stmt);
+                    switch(result_sql_query){
+                        case SQLITE_OK:{
+                            code=code_succes;
+                            break;
+                        }
+                        case SQLITE_DONE:{
+                            code =code_fail;
+                            break;
+                        }
+                        default:{
+                            code=code_eror;
+                            break;
+                        }
+                    }
                 }
                 break;
             }
             case 2:{
-                string sql="SELECT "+SearchTerm+" FROM "+table+" WHERE "+key_1+" = ? AND "+key_2+"= ?;";
+                string sql="SELECT "+SearchTerm+" FROM "+table+" WHERE "+key_1+" = :sqlfilter_2 AND "+key_2+"= :sqlfilter_2;";
                 if (sqlite3_prepare_v2(db,sql.c_str(),-1,&stmt,nullptr)==SQLITE_OK) {
-                
+                    int index_filter_1 = sqlite3_bind_parameter_index(stmt,":sqlfilter_1");
+                    int index_filter_2 = sqlite3_bind_parameter_index(stmt,":sqlfilter_2");
+                    sqlite3_bind_text(stmt,index_filter_1,filter_1.c_str(),-1,SQLITE_STATIC);
+                    sqlite3_bind_text(stmt,index_filter_2,filter_2.c_str(),-1,SQLITE_STATIC);
+                    int result_sql_query =sqlite3_step(stmt);
+                    switch(result_sql_query){
+                        case SQLITE_OK:{
+                            code=code_succes;
+                            break;
+                        }
+                        case SQLITE_DONE:{
+                            code =code_fail;
+                            break;
+                        }
+                        default:{
+                            code=code_eror;
+                            break;
+                        }
+                    }
                 }
                 break;
             }
         }
+    sqlite3_finalize(stmt);
+    sqlite3_close(db);
+    return code;
     }
 };
